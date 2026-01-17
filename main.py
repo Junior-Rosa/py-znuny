4f21be3798474739e5724c9b671de2cf76d7adb0
Junior Rosa <jr.dasrosas@gmail.com>
2026-01-17 12:50:44 -0300

    refactor: docs

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from py-znuny!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/main.py
@@ -0,0 +1,9 @@
+def main(msg: str = "") -> None:
+    """Ponto de entrada principal do CLI.
+
+    :param msg: Mensagem opcional a ser exibida.
+    """
+    print("Hello from py-znuny!" + msg)
+
+if __name__ == "__main__":
+    main()
